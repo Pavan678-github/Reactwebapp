6d3f0f081f63edec3d2c3cf61de5b54ead0de9dc
betul-prog <btlkurumsalbtl@gmail.com>
2021-12-08 14:37:55 +0300

    REACTWEBAPP

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './assets/css/App.css';
+const addTwoNumbers1 = (number1, number2) => {
+  return number1 + number2
+}
+
+const addTwoNumbers2 = (number1, number2) => number1 + number2
+
+function addTwoNumbers3(number1, number2) {
+  return number1 + number2;
+}
+// App Functional Component
+function App1() {
+  return (
+    <div className="App">
+      <p>
+        Result : {addTwoNumbers1(3, 5)}
+      </p>
+    </div>
+  );
+}
+//Yukarısı ile aynı ifade
+const App2 = () => (
+  <div className="App">
+    <p>
+      Result : {addTwoNumbers1(3, 5)}
+    </p>
+  </div>
+);
+
+//Yukarısı ile aynı ifade
+const App2 = () => {
+  return (
+    <div className="App">
+      <p>Result : {addTwoNumbers1(3, 5)}</p>
+    </div>
+  );
+}
+//Her sayfa için Default exportta tek function export yapılır
+//const olarak verilen değişkenleri inline default export yapılamaz.
+//ama aşağıdaki gibi dafult export yapılabilir...
+export default App;
